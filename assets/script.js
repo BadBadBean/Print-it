76183e08761ab2e448d43396c4169662d72f09f2
Delphine <mallet.delphine@hotmail.fr>
2024-10-01 03:07:46 +0200

    adding functional left/ right click and active bullet point

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,70 @@
-// All slides
+// Carousel images
 
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "./assets/images/slideshow/slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "./assets/images/slideshow/slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
+// Variables
 
-// Arrows
 const prevButton = document.querySelector(".arrow_left");
 const nextButton = document.querySelector(".arrow_right");
-
-prevButton.addEventListener("click", () => {
-	console.log("clic sur le bouton gauche")
-});
-
-nextButton.addEventListener("click", () => {
-	console.log("clic sur le bouton droit")
-});
+const slideImage = document.querySelector(".banner-img");
+const tagLine = document.querySelector("#banner p");
+const dots = document.querySelector(".dots");
+const numberOfSlides = slides.length;
+let index = 0;
 
 // Bullet points
 
-// 1. Sélectionner l'élément conteneur des bullets
-const dots = document.querySelector(".dots");
-
-// 2. Définir le nombre de slides
-const numberOfSlides = slides.length;
-
-
-// 3. Créer les bullets points
 for (let i = 0; i < numberOfSlides; i++) {
-  // Créer un élément bullet (span ou button)
- let dot = document.createElement("div");
- // Ajouter une classe pour styliser chaque bullet
- dot.classList.add('dot');
- // Ajoute les dots au conteneur
- dots.appendChild(dot);
-
-// 4. Le premier dot est actif par défaut
+  let dot = document.createElement("div");
+  dot.classList.add("dot");
+  dots.appendChild(dot);
   if (i === 0) {
-    dot.classList.add('dot_selected');
+    dot.classList.add("dot_selected");
   }
 }
 
-let currentSlide = 0;
+// Left click
 
-function moveSlide(index) {
-	const slideImage = document.querySelector(".banner-img");
-}
+prevButton.addEventListener("click", () => {
+  const activeDot = document.querySelectorAll(".dots .dot");
+  activeDot[index].classList.remove("dot_selected");
+  index--;
+  if (index < 0) {
+    index = activeDot.length - 1;
+  }
+  slideImage.src = slides[index].image;
+  tagLine.innerHTML = slides[index].tagLine;
+  activeDot[index].classList.add("dot_selected");
+});
+
+// Right click
+
+nextButton.addEventListener("click", () => {
+  const activeDot = document.querySelectorAll(".dots .dot");
+  activeDot[index].classList.remove("dot_selected");
+  index++;
+  if (index > slides.length - 1) {
+    index = 0;
+  }
+  slideImage.src = slides[index].image;
+  tagLine.innerHTML = slides[index].tagLine;
+  activeDot[index].classList.add("dot_selected");
+});
